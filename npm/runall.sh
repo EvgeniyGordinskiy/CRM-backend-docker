ce9300f9a310fb12364cfd6217997f74003cf45e
EvgeniyGordinskiy <evgeniy@gordinskiy.com>
2018-06-04 20:07:56 +0300

    Rename npm to vue and added workdir for vue

--- npm/runall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Running /data/crm_docker/httpdocs_node/ "
-cd "/data/crm_docker/httpdocs_node/"
-npm install
-npm run dev
---
+++ vue/runall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Running /data/crm_docker/httpdocs_vue/ "
+cd "/data/crm_docker/httpdocs_vue/"
+npm install
+npm run dev
